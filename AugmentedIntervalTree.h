65144ff702659d00398107bf28ce4e821bc2e1ef
GlenMerlin <Glenmerlin@protonmail.com>
2022-03-13 21:49:25 -0600

    I am completely lost

--- AugmentedIntervalTree.h
+++ AugmentedIntervalTree.h
@@ -33,12 +33,25 @@ class AugmentedIntervalTree : public IntervalTree<T> {
         
         bool add(T const& lower, T const& upper){
             // refactor the update data function to handle upper and lower properly
-            return updateData(nullptr, lower, upper);
+            return updateData(root, lower, upper);
         };
 
-        bool updateData(Node<T>*& currentnode, T lower, T upper){
-            // algorithm needs work (but should be similar to Lab7)
-            return false;
+        bool updateData(Node<T>*& currentNode, T lower, T upper){
+            if (currentNode == NULL){
+                currentNode->interval = new Node<T>(new Interval<T>(lower, upper));
+                return true;
+            }
+            else {
+                // if (data < currentNode->){
+                //     return updateData(currentNode->left, lower, upper);
+                // }
+                // else if (data > currentNode->data) {
+                //     return updateData(currentNode->right, lower, upper);
+                // }
+                // else {
+                    return false;
+                // }
+            }
         }
         
         vector<Interval<T>> query(T const& point) const{
@@ -48,7 +61,7 @@ class AugmentedIntervalTree : public IntervalTree<T> {
         };
         
         bool remove(T const& lower, T const& upper){
-            return deleteData(nullptr, lower, upper);
+            return deleteData(root, lower, upper);
         };
         
         bool deleteData(Node<T>*& currentNode, T lower, T upper){
