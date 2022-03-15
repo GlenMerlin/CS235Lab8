0c4a950ac1349ed396da36214ef76c03dc252e10
GlenMerlin <Glenmerlin@protonmail.com>
2022-03-15 09:26:06 -0600

    test functions now actually run

--- AugmentedIntervalTree.h
+++ AugmentedIntervalTree.h
@@ -6,39 +6,39 @@ class AugmentedIntervalTree : public IntervalTree<T> {
     public: 
         AugmentedIntervalTree(){
             cout << "AIT Constructor" << endl;
-            root = nullptr;
         };
         
         virtual ~AugmentedIntervalTree(){
             cout << "AIT Constructor" << endl;
-            delete root;
+            clear();
         };
-        Node<T>* root;
+
         void clear(){
-            clearHelper(root);
+            clearHelper(this->root);
         };
 
         void clearHelper(Node<T>*& currentNode){
             while (currentNode != nullptr){
-                clearHelper(currentNode->leftChild);
-                clearHelper(currentNode->rightChild);
+                clearHelper(currentNode->left);
+                clearHelper(currentNode->right);
                 delete currentNode;
                 currentNode = nullptr;
             }
         };
         
-        bool is_empty(){
-            return root == nullptr;
+        bool is_empty() const override {
+            return this->root == nullptr;
         };
         
         bool add(T const& lower, T const& upper){
             // refactor the update data function to handle upper and lower properly
-            return updateData(root, lower, upper);
+            return updateData(this->root, lower, upper);
         };
 
         bool updateData(Node<T>*& currentNode, T lower, T upper){
             if (currentNode == NULL){
-                currentNode->interval = new Node<T>(new Interval<T>(lower, upper));
+                Interval<T> tmp(lower, upper);
+                currentNode = new Node<T>(tmp);
                 return true;
             }
             else {
@@ -53,15 +53,40 @@ class AugmentedIntervalTree : public IntervalTree<T> {
                 // }
             }
         }
+
+        // void updateMinMax(Node<T>*& node){
+        //     if (node == nullptr){
+        //         return;
+        //     }
+        //     node->min_max = 1 + max(
+        //         node->left == nullptr ? -1 : node->left->min_max,
+        //         node->right == nullptr? -1 : node->right->min_max
+        //     )
+        // }
         
         vector<Interval<T>> query(T const& point) const{
             // This function is basically a binary search to see which of the different nodes match the query
-            vector<Interval<T>> nodes = NULL;
+            vector<Interval<T>> nodes;
+            // queryHelper(this->root, point, nodes);
             return nodes;
         };
+
+        // void queryHelper(Node<T>* const& node, T point, vector<T>& nodes){
+        //     // if (node == nullptr){
+        //     //     return;
+        //     // }
+
+        //     // queryHelper(node->left, point, nodes);
+        //     // // definitely wrong check but fix later
+        //     // if(abs(item - node->interval) <= 3){
+        //     //     nodes.push_back(node->interval)
+        //     // }
+
+        //     // queryHelper(node->right, point, items_found);
+        // }
         
         bool remove(T const& lower, T const& upper){
-            return deleteData(root, lower, upper);
+            return deleteData(this->root, lower, upper);
         };
         
         bool deleteData(Node<T>*& currentNode, T lower, T upper){
--- tests.cpp
+++ tests.cpp
@@ -5,6 +5,8 @@ using namespace std;
 // This file is designed to perform tests on our functions to make sure that they're working properly
 
 int main(){
+    AugmentedIntervalTree<int> tree;
+    test("Empty Check", tree.is_empty(), false);
     return 0;
 }
 
